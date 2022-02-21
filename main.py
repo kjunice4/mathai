2638117768c39d18076ce777d9e790693997e551
Kevin Junice <37306150+kjunice4@users.noreply.github.com>
2022-02-20 19:41:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,16 +26,27 @@ Builder.load_string("""
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
-                
+        
         Button:
             font_size: 50
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
             height: 200
-            text: "KSquared-math,LLC ©"
+            text: "Tap anywhere to continue"
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
+        
+        Button:
+            font_size: 50
+            background_color: 0, 0 , 0 , 1
+            size_hint_y: None
+            height: 200
+            text: "KSquared-math,LLC © : All applications"
+            on_release:
+                app.root.current = "Menu"
+                root.manager.transition.direction = "left" 
+                
 """)
 
 # Menu
