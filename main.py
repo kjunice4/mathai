df390e2047d3f620b1e6f8e3b73de2fca0eb5ff1
Kevin Junice <37306150+kjunice4@users.noreply.github.com>
2022-03-29 18:37:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7223,9 +7223,10 @@ class Domain_and_Range(Screen):
         Window.bind(on_keyboard=self._key_handler)
     
     def _key_handler(self, instance, key, *args):
-        print("Key:",key)
+        print("Key:",type(key))
         self.ids.list_of_steps.add_widget(Label(text= "Key: " + str(key) ,font_size = 60, size_hint_y= None, height=100))
-        if key == 27:
+        
+        if str(key) == str(27):
             self.set_previous_screen()
             return True
     
